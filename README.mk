fe7ce8a5372736672c2729162a575ce3a887c747
Jason <bjason@gmail.com>
2014-10-20 21:35:31 -0700

    better ruby isntall

--- README.mk
+++ README.mk
@@ -1,6 +1,8 @@
 
 ## Installing Ruby:
 
+
+DOES NOT WORK
 ```
 curl -L get.rvm.io | bash -s stable
 
@@ -17,6 +19,14 @@ rvm list
 
 rvm alias create default ruby-1.9.3-p547
 
+
+```
+
+
+Installing ruby:
+
+```
+sudo apt-get install ruby 1.9.3
 
 ```
 
